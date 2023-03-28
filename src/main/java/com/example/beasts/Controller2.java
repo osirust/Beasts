a553ab7522d01396ca9781d9b79b53d4c911ca9e
unknown <java1.4@KVANTORIUM69.LOCAL>
2023-03-28 17:58:57 +0300

    Initial commit

---
+++ src/main/java/com/example/beasts/Controller2.java
@@ -0,0 +1,60 @@
+package com.example.beasts;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class Controller2 {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Button BobrBut;
+
+    @FXML
+    private Button ByrozybkaBut;
+
+    @FXML
+    private Button EjBut;
+
+    @FXML
+    private Button KabanBut;
+
+    @FXML
+    private Button KrasaBut;
+
+    @FXML
+    private Button MashovkaBut;
+
+    @FXML
+    private Button MedvedBut;
+
+    @FXML
+    private Button NochnicaBut;
+
+    @FXML
+    private Button Ondatra;
+
+    @FXML
+    private Button RabbitBut;
+
+    @FXML
+    private Button RasBut;
+
+    @FXML
+    private Button SoniaBut;
+
+    @FXML
+    private Button exitBut;
+
+    @FXML
+    void initialize() {
+
+    }
+
+}
---
+++ src/main/java/com/example/beasts/HelloApplication.java
@@ -0,0 +1,24 @@
+package com.example.beasts;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class HelloApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("asdfgg.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(),1255 , 852);
+        stage.setTitle("Beasts");
+        stage.setResizable(false);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
